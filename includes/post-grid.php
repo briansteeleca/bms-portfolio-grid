855c5b3fc20bf909d89d4e937f0704430d4462cd
Brian Steele <steeleware@gmail.com>
2016-08-30 12:19:39 -0400

    Update post grid template for flip card effect.
    Notes:
    - uses ACF plugin for website_url and twitter_url fields
    - uses custom except function (in functions.php) to limit length

--- includes/post-grid.php
+++ includes/post-grid.php
@@ -1,57 +1,61 @@
-<div class="fl-post-grid-post" itemscope itemtype="<?php FLPostGridModule::schema_itemtype(); ?>">
-	
-	<?php FLPostGridModule::schema_meta(); ?>
+<?php
 
-	<?php if(has_post_thumbnail() && $settings->show_image) : ?>
-	<div class="fl-post-grid-image">
-		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+$post_id = get_the_ID();
+
+$terms = get_the_terms($post_id, 'portfolio_category' );
+if ($terms && ! is_wp_error($terms)) :
+	$term_slugs_arr = array();
+	foreach ($terms as $term) {
+	    $term_slugs_arr[] = $term->slug;
+	}
+	$terms_slug_str = join( " ", $term_slugs_arr);
+endif;
+?>
+
+<div class="fl-post-grid-post bms-portfolio-grid-container element-item <?php echo $terms_slug_str; ?>" itemscope itemtype="<?php ZSUPortfolioGridModule::schema_itemtype(); ?>">
+
+	<?php //echo $terms_slug_str; ?>
+
+	<?php BMSPortfolioGridModule::schema_meta(); ?>
+
+	<div class="flipper">
+
+		<?php if(has_post_thumbnail() && $settings->show_image) : ?>
+		<div class="fl-post-grid-image bms-portfolio-grid-front">
 			<?php the_post_thumbnail($settings->image_size); ?>
-		</a>
-	</div>
-	<?php endif; ?>
+		</div>
+		<?php endif; ?>
 
-	<div class="fl-post-grid-text">
+		<div class="fl-post-grid-text bms-portfolio-grid-back">
 
-		<h2 class="fl-post-grid-title" itemprop="headline">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-		</h2>
+			<h2 class="fl-post-grid-title" itemprop="headline">
+				<?php the_title(); ?>
+			</h2>
 
-		<?php if($settings->show_author || $settings->show_date) : ?>
-		<div class="fl-post-grid-meta">
-			<?php if($settings->show_author) : ?>
-				<span class="fl-post-grid-author">
-				<?php
-
-				printf(
-					_x( 'By %s', '%s stands for author name.', 'fl-builder' ),
-					'<a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '"><span>' . get_the_author_meta( 'display_name', get_the_author_meta( 'ID' ) ) . '</span></a>'
-				);
-
-				?>
-				</span>
-			<?php endif; ?>
-			<?php if($settings->show_date) : ?>
-				<?php if($settings->show_author) : ?>
-					<span> | </span>
+			<?php if($settings->show_content || $settings->show_more_link) : ?>
+			<div class="fl-post-grid-content">
+				<?php if($settings->show_content) : ?>
+				<?php the_excerpt(21); ?>
 				<?php endif; ?>
-				<span class="fl-post-feed-date">
-					<?php FLBuilderLoop::post_date($settings->date_format); ?>
-				</span>
-			<?php endif; ?>
-		</div>
-		<?php endif; ?>
+			</div>
 
-		<?php if($settings->show_content || $settings->show_more_link) : ?>
-		<div class="fl-post-grid-content">
-			<?php if($settings->show_content) : ?>
-			<?php the_excerpt(); ?>
-			<?php endif; ?>
-			<?php if($settings->show_more_link) : ?>
-			<a class="fl-post-grid-more" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php echo $settings->more_link_text; ?></a>
-			<?php endif; ?>
-		</div>
-		<?php endif; ?>
+            <div class="bms-portfolio-grid-website" >
+                <?php if($settings->show_more_link) : ?>
+                <a class="fl-post-grid-more" href="<?php the_field( 'website_url' ); ?>" title="<?php the_title_attribute(); ?>"><?php echo $settings->more_link_text; ?></a>
+                <?php endif; ?>
+            </div>
 
-	</div>
+            <div class="bms-portfolio-grid-social">
+                <a href="<?php the_field( 'twitter_url' ); ?>" target="_self">
+                    <span class="fl-icon-wrap">
+                        <span class="fl-icon">
+                            <i class="fa fa-twitter"></i> 
+                        </span>
+                    </span>
+                </a>  
+            </div>
+			<?php endif; ?>
 
+		</div><!-- bms-portfolio-grid-back -->
+    </div><!-- flipper -->
 </div>
